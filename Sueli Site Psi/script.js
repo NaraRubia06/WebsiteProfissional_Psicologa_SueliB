f9b90af740473dacbf443a047a73552ec8acf6a5
NaraRubia06 <127237405+NaraRubia06@users.noreply.github.com>
2025-11-16 10:37:00 -0300

    Add files via upload

---
+++ Sueli Site Psi/script.js
@@ -0,0 +1,6 @@
+document.getElementById('agendarConsulta').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/5545998229090?text=Olá,%20gostaria%20de%20agendar%20uma%20consulta.';
+});
+document.getElementById('agendarConsulta2').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/5545998229090?text=Olá,%20gostaria%20de%20agendar%20uma%20consulta.';
+});
